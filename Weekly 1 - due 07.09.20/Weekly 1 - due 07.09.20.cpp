60bb261b7a2a8818262cc3df7b211f6b7b1b0f1e
Bernt Leander Vargan <b.leandervargan@gmail.com>
2020-09-01 17:04:39 +0200

    Add project files.

---
+++ Weekly 1 - due 07.09.20/Weekly 1 - due 07.09.20.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+std::string name = "";
+std::string dob = ""; //Date of birth
+int age;
+long phone;
+
+int main()
+{
+    // Get information
+
+    std::cout << "Hello there, please write your name\n";
+    std::cin >> name;
+    std::string sSource = name;
+    std::cout << "\nHi, " << name << ". How old are you?\n";
+    std::cin >> age;
+    std::cout << "\nOkay.\nNow, what's your phone number?\n";
+    std::cin >> phone;
+    std::cout << "\nRight.\nLastly, I'll need your date of birth (dd/mm/yyyy)\n";
+    std::cin >> dob;
+
+    //std::string sSource = name;
+    //std::cout << sSource[0] << "\n";
+
+    // Print information
+
+    std::cout << "\n-------------------------\n";
+    std::cout << " | Name     | " << name << "\n";
+    std::cout << " | Initial  | " << sSource[0] << "\n";
+    std::cout << " | Age      | " << age << "\n";
+    std::cout << " | Phone nr | " << phone << "\n";
+    std::cout << " | Birthday | " << dob << "\n";
+    std::cout << "-------------------------\n";
+}
+
